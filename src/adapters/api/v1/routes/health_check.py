d80e695bd88658db86d29566ecd00aad57c7285b
Carlos Roberto <carlosjr.if@gmail.com>
2024-12-09 08:14:07 -0300

    feat: add response model on route health check

--- src/adapters/api/v1/routes/health_check.py
+++ src/adapters/api/v1/routes/health_check.py
@@ -1,9 +1,10 @@
 from http import HTTPStatus
+from src.schemas.health_check_schema import HealthCheckSchemaOut
 from src.application.usecases.health_check_usecase.health_check_usecase import HealthCheckUseCase
 from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/health", status_code=HTTPStatus.OK)
+@router.get("/health", response_model=HealthCheckSchemaOut, status_code=HTTPStatus.OK)
 async def health_check():
     return HealthCheckUseCase().execute()
