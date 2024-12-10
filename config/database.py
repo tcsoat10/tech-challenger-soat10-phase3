a410a954b1e56d024a09ff866f0d49f903c1d683
Carlos Roberto <carlosjr.if@gmail.com>
2024-12-10 19:26:40 -0300

    feat: initial config alembic

---
+++ config/database.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+# Obter configurações do banco de dados diretamente das variáveis de ambiente
+DATABASE = {
+    "drivername": "mysql+pymysql",
+    "host": os.getenv("MYSQL_HOST", "localhost"),
+    "port": os.getenv("MYSQL_PORT", "3306"),
+    "user": os.getenv("MYSQL_USER", "user"),
+    "password": os.getenv("MYSQL_PASSWORD", "password"),
+    "name": os.getenv("MYSQL_DATABASE", "db_name"),
+}
+
+# Construir a URL do banco de dados
+DATABASE_URL = (
+    f"{DATABASE['drivername']}://{DATABASE['user']}:{DATABASE['password']}@"
+    f"{DATABASE['host']}:{DATABASE['port']}/{DATABASE['name']}"
+)
+
+# Criar o motor do SQLAlchemy
+engine = create_engine(DATABASE_URL, echo=True)
+
+# Configurar a sessão
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Classe base para os modelos
+Base = declarative_base()
