d4e914bb9139ddbb7922348c49561bcf5f655e11
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2024-12-28 21:49:44 -0300

    feat: add methods implementation to permission repository

--- src/adapters/driven/repositories/permission_repository.py
+++ src/adapters/driven/repositories/permission_repository.py
@@ -1,6 +1,8 @@
 from src.core.domain.entities.permission import Permission
 from src.core.ports.permission.i_permission_repository import IPermissionRepository
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import exists
+from typing import List
 
 class PermissionRepository(IPermissionRepository):
 
@@ -12,3 +14,26 @@ class PermissionRepository(IPermissionRepository):
         self.db_session.commit()
         self.db_session.refresh(permission)
         return permission
+    
+    def exists_by_name(self, name: str) -> bool:
+        return self.db_session.query(exists().where(Permission.name) == name).scalar()
+    
+    def get_by_name(self, name: str) -> Permission:
+        return self.db_session.query(Permission).filter(Permission.name == name).first()
+    
+    def get_by_id(self, permission_id: int) -> Permission:
+        return self.db_session.query(Permission).filter(Permission.id == permission_id).first()
+    
+    def get_all(self) -> List[Permission]:
+        return self.db_session.query(Permission).all()
+    
+    def update(self, permission: Permission) -> Permission:
+        self.db_session.merge(Permission)
+        self.db_session.commit()
+        return permission
+    
+    def delete(self, permission_id: int) -> None:
+        permission = self.get_by_id(permission_id=permission_id)
+        if permission:
+            self.db_session.delete(permission)
+            self.db_session.commit()
