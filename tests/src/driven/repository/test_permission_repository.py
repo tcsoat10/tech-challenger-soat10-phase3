799074611d30fbb8ff1a0f18c0d0563aa20c5d76
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2024-12-30 23:10:00 -0300

    tests: add tests to permission repository

--- tests/src/driven/repository/test_permission_repository.py
+++ tests/src/driven/repository/test_permission_repository.py
@@ -1,6 +1,7 @@
 import pytest
 from src.adapters.driven.repositories.permission_repository import PermissionRepository
 from src.core.domain.entities.permission import Permission
+from sqlalchemy.exc import IntegrityError
 
 
 class TestPermissionRepository:
@@ -19,20 +20,20 @@ class TestPermissionRepository:
 
     def test_create_permission_success(self):
         # Criando a permissão "Admin"
-        admin_permission = Permission(name="Admin", description="System admin permission")
+        admin_permission = Permission(name="Admin", description="System admin privileges")
         created_admin_permission = self.repository.create(admin_permission)
 
         assert created_admin_permission.id is not None
         assert created_admin_permission.name == "Admin"
-        assert created_admin_permission.description == "System admin permission"
+        assert created_admin_permission.description == "System admin privileges"
 
         # Criando a permissão "Employee"
-        employee_permission = Permission(name="Employee", description="System user permission")
+        employee_permission = Permission(name="Employee", description="System user privileges")
         created_employee_permission = self.repository.create(employee_permission)
 
         assert created_employee_permission.id is not None
         assert created_employee_permission.name == "Employee"
-        assert created_employee_permission.description == "System user permission"
+        assert created_employee_permission.description == "System user privileges"
 
         # Verifica se a permissão Admin foi persistida no banco
         db_permission = self.db_session.query(Permission).filter_by(name="Admin").first()
@@ -43,3 +44,141 @@ class TestPermissionRepository:
         db_permission = self.db_session.query(Permission).filter_by(name="Employee").first()
         assert db_permission is not None
         assert db_permission.name == "Employee"
+    
+    def test_create_duplicate_permission_return_error(self, db_session):
+        '''
+        Testa erro ao criar uma permissão com o nome duplicado
+        '''
+
+        # Criando a primeira permissão
+        new_permission = Permission(name="Admin", description="System admin privileges")
+        self.repository.create(new_permission)
+
+        # Tentando criar a permissão duplicada
+        duplicate_permission = Permission(name="Admin", description="New one")
+        with pytest.raises(IntegrityError):
+            self.repository.create(duplicate_permission)
+
+    def test_get_permission_by_name_success(self):
+        '''
+        Testa a recuperação de uma permissão pelo nome com sucesso
+        '''
+
+        # Criando a primeira permissão
+        new_permission = Permission(name="Admin", description="System admin privileges")
+        created_permission = self.repository.create(new_permission)
+
+        permission = self.repository.get_by_name(created_permission.name)
+
+        assert permission is not None
+        assert permission.id == created_permission.id
+        assert permission.name == created_permission.name
+        assert permission.description == created_permission.description
+
+    def test_get_permission_by_name_with_name_not_registered(self):
+        '''
+        Testa a busca de uma permissão com um nome que não está registrado
+        '''
+
+        # Criando a primeira permissão
+        new_permission = Permission(name="Admin", description="System admin privileges")
+        self.repository.create(new_permission)
+
+        permission = self.repository.get_by_name('not a name')
+
+        assert permission is None
+    
+    def test_get_permission_by_id_success(self):
+        '''
+        Testa a recuperação de uma permissão pelo id
+        '''
+
+        # Criando a primeira permissão
+        new_permission = Permission(name="Admin", description="System admin privileges")
+        created_permission = self.repository.create(new_permission)
+
+        permission = self.repository.get_by_id(created_permission.id)
+
+        assert permission is not None
+        assert permission.id == created_permission.id
+        assert permission.name == created_permission.name
+        assert permission.description == created_permission.description
+
+    def test_get_permission_by_id_with_id_not_registered(self):
+        '''
+        Testa a busca de uma permissão com um id não registrado
+        '''
+
+        # Criando a primeira permissão
+        new_permission = Permission(name="Admin", description="System admin privileges")
+        self.repository.create(new_permission)
+
+        permission = self.repository.get_by_id(2)
+
+        assert permission is None
+    
+    def test_get_all_permissions(self):
+        '''
+        Testa a busca de todas as permissões
+        '''
+
+        # Criando as permissões
+        permission1 = Permission(name="Admin", description="System admin privileges")
+        permission2 = Permission(name="Employee", description="System user privileges")
+
+        self.repository.create(permission1)
+        self.repository.create(permission2)
+
+        permissions = self.repository.get_all()
+
+        assert len(permissions) == 2
+        assert permissions[0].name == 'Admin'
+        assert permissions[1].name == 'Employee'
+    
+    def test_get_all_permissions_with_empty_db(self):
+        '''
+        Testa a busca de todas as permissões com o banco de dados vazio
+        '''
+
+        permissions = self.repository.get_all()
+        assert len(permissions) == 0
+        assert permissions == []
+    
+    def test_update_permission(self):
+        '''
+        Testa a atualização de uma permissão
+        '''
+
+        permission = Permission(name="Admin", description="System admin privileges")
+        created_permission = self.repository.create(permission)
+
+        created_permission.name = 'Admin - updated'
+        created_permission.description = 'System admin privileges - updated'
+
+        updated_permission = self.repository.update(created_permission)
+
+        assert updated_permission.name == created_permission.name
+        assert updated_permission.description == created_permission.description
+
+    def test_delete_permission(self):
+        '''
+        Testa a delação de uma permissão
+        '''
+
+        permission = Permission(name="Admin", description="System admin privileges")
+        created_permission = self.repository.create(permission)
+
+        self.repository.delete(created_permission.id)
+
+        assert len(self.repository.get_all()) == 0
+
+    def test_delete_permission_with_inexistent_id(self):
+        permission = Permission(name="Admin", description="System admin privileges")
+        self.repository.create(permission)
+
+        self.repository.delete(2)
+
+        assert len(self.repository.get_all()) == 1
+
+        
+        
