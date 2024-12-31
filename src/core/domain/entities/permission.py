efdb041e888f9e3e94b36a8c27a13d9fa90d3617
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2024-12-30 22:43:33 -0300

    fix: make Permission entity name unique

--- src/core/domain/entities/permission.py
+++ src/core/domain/entities/permission.py
@@ -5,5 +5,5 @@ from src.core.domain.entities.base_entity import BaseEntity
 class Permission(BaseEntity):
     __tablename__ = "permissions"
 
-    name = Column(String(100), nullable=False)
+    name = Column(String(100), nullable=False, unique=True)
     description = Column(String(300))
