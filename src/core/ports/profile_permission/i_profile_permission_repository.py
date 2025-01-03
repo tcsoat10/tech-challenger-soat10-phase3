1620c962ec0ec03f57bf130d2c617e23b93d0f89
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-01-02 22:27:21 -0300

    feat: add interface profile_permission repository

---
+++ src/core/ports/profile_permission/i_profile_permission_repository.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from src.core.domain.entities.profile_permission import ProfilePermission
+
+
+class IProfilePermissionRepository(ABC):
+    @abstractmethod
+    def create(profile_permission: ProfilePermission):
+        pass
+    
+    @abstractmethod
+    def get_by_id(self, profile_permission_id: int) -> ProfilePermission:
+        pass
+
+    @abstractmethod
+    def get_by_profile_id(self, profile_id: int) -> ProfilePermission:
+        pass
+
+    @abstractmethod
+    def get_by_permission_id(self, permission_id: int) -> ProfilePermission:
+        pass
+
+    @abstractmethod
+    def get_all(self) -> List[ProfilePermission]:
+        pass
+
+    @abstractmethod
+    def update(self, profile_permission: ProfilePermission) -> ProfilePermission:
+        pass
+
+    @abstractmethod
+    def delete(self, profile_permission: ProfilePermission) -> None:
+        pass
+
+__all__ = ['IProfilePermissionRepository']
