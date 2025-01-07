b964347018628ebc1f68ef2e149d86752515d160
Carlos Roberto <carlosjr.if@gmail.com>
2025-01-06 21:35:09 -0300

    feat: reactivate products

--- tests/src/adapters/driver/api/v1/routes/test_product_routes.py
+++ tests/src/adapters/driver/api/v1/routes/test_product_routes.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from fastapi import status
 
 import pytest
@@ -46,6 +47,30 @@ def test_create_product_duplicate_name_and_return_error(client, db_session):
     data = response.json()
 
     assert data == {"error": "Product already exists."}
+
+def test_reactivate_product_and_return_success(client, db_session):
+    category = CategoryFactory(name="Drinks")
+    ProductFactory(name="Coca-Cola", category=category, inactivated_at=datetime.now())
+
+    payload = {
+        "name": "Coca-Cola",
+        "description": "Soft drink",
+        "price": 6.99,
+        "category_id": category.id,
+    }
+
+    response = client.post("/api/v1/products", json=payload)
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+    data = response.json()
+
+    assert "id" in data
+    assert data["name"] == payload["name"]
+    assert data["description"] == payload["description"]
+    assert data["price"] == payload["price"]
+    assert data["category"]["id"] == category.id
+    assert data["category"]["name"] == category.name
 
 
 def test_get_product_by_name_and_return_success(client):
