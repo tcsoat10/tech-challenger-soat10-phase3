ea3b9b1f8cee6a32ee4c213ff842e4f1e0e3f6c5
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-01-11 01:06:35 -0300

    feat: add person entity

---
+++ src/core/domain/entities/person.py
@@ -0,0 +1,12 @@
+from src.core.domain.entities.base_entity import BaseEntity
+from sqlalchemy import Column, String, Date
+
+
+class Person(BaseEntity):
+    __tablename__ = "persons"
+
+    name = Column(String(100))
+    cpf = Column(String(11), unique=True, nullable=False)
+    email = Column(String(150), unique=True)
+    birth_date = Column(Date)
+    
