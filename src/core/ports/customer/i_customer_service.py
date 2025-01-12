ce7ea3d1ed6670bd17e7ebb982d8b37bfb979769
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-01-12 15:52:18 -0300

    feat: add customer service and interface

---
+++ src/core/ports/customer/i_customer_service.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from src.core.domain.dtos.customer.create_customer_dto import CreateCustomerDTO
+from src.core.domain.dtos.customer.customer_dto import CustomerDTO
+from src.core.domain.dtos.customer.update_customer_dto import UpdateCustomerDTO
+
+
+
+class ICustomerService(ABC):
+    @abstractmethod
+    def create_customer(self, dto: CreateCustomerDTO) -> CustomerDTO:
+        pass
+    
+    @abstractmethod
+    def get_customer_by_id(self, customer_id: int) -> CustomerDTO:
+        pass
+
+    @abstractmethod
+    def get_customer_by_person_id(self, customer_id: int) -> CustomerDTO:
+        pass
+
+    @abstractmethod
+    def get_all_customers(self, include_deleted: bool = False) -> List[CustomerDTO]:
+        pass
+
+    @abstractmethod
+    def update_customer(self, customer_id: int, dto: UpdateCustomerDTO) -> CustomerDTO:
+        pass
+
+    @abstractmethod
+    def delete_customer(self, customer_id: int) -> None:
+        pass
