17ef792c5c5cc497bb62790a4c2b691e9b2b50de
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-01-12 15:51:31 -0300

    fix: merge alembic heads

---
+++ migrations/versions/b8bbe643e87a_merge_heads_97c5618569bf_and_d0843291c761.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: b8bbe643e87a
+Revises: 97c5618569bf, d0843291c761
+Create Date: 2025-01-12 12:14:44.706667
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'b8bbe643e87a'
+down_revision: Union[str, None] = ('97c5618569bf', 'd0843291c761')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
