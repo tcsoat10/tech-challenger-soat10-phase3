d7229500734e6e48abe98f3f948bcc0fd61b500c
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-01-13 00:30:50 -0300

    feat: add entity employee

---
+++ src/core/domain/entities/employee.py
@@ -0,0 +1,23 @@
+from src.core.domain.entities.base_entity import BaseEntity
+
+from sqlalchemy import Column, Date, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Employee(BaseEntity):
+    __tablename__ = 'employess'
+
+    admission_date = Column(Date)
+    termination_date = Column(Date)
+
+    person_id = Column(ForeignKey('persons.id'), nullable=False)
+    person = relationship('Person')
+
+    role_id = Column(ForeignKey('roles.id'), nullable=False)
+    role = relationship('Role')
+
+    user_id = Column(ForeignKey('users.id'), nullable=False)
+    user = relationship('User')
+
+
+__all__ = ['Employee']
