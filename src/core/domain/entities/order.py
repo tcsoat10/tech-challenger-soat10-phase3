1d6bc0bcaf3e5b5eb321b9ed7880d01d3cf4ccea
Carlos Roberto <carlosjr.if@gmail.com>
2025-01-18 23:00:16 -0300

    refactor: change methods to advance order to next status

--- src/core/domain/entities/order.py
+++ src/core/domain/entities/order.py
@@ -1,17 +1,21 @@
-from typing import Optional, TYPE_CHECKING
+from typing import List, Optional
 from sqlalchemy import Column, ForeignKey, Integer
 from sqlalchemy.orm import relationship
 
-from src.core.ports.order_status.i_order_status_repository import IOrderStatusRepository
-from src.core.domain.entities.order_item import OrderItem
+from core.domain.entities.employee import Employee
+from core.domain.entities.order_status import OrderStatus
+from core.domain.entities.order_item import OrderItem
 from src.core.exceptions.bad_request_exception import BadRequestException
 from src.constants.order_status import OrderStatusEnum
 from .base_entity import BaseEntity
 
-if TYPE_CHECKING:
-    from src.core.ports.order.i_order_repository import IOrderRepository
+STATUS_TRANSITIONS = {
+    OrderStatusEnum.ORDER_PENDING: OrderStatusEnum.ORDER_PLACED,
+    OrderStatusEnum.ORDER_PLACED: OrderStatusEnum.ORDER_PREPARING,
+    OrderStatusEnum.ORDER_PREPARING: OrderStatusEnum.ORDER_READY,
+    OrderStatusEnum.ORDER_READY: OrderStatusEnum.ORDER_COMPLETED,
+}
 
-# tabelas
 class Order(BaseEntity):
     __tablename__ = 'orders'
 
@@ -23,91 +27,102 @@ class Order(BaseEntity):
     employee = relationship('Employee')
 
     order_items = relationship('OrderItem', back_populates='order', cascade='all, delete-orphan')
-    
+
     @property
-    def order_status_name(self):
+    def order_status_name(self) -> str:
         return self.order_status.status
 
     @property
-    def customer_person(self):
-        return self.customer.person
+    def customer_person(self) -> Optional[str]:
+        return self.customer.person if self.customer else None
+
+    @property
+    def employee_name(self) -> Optional[str]:
+        return self.employee.person.name if self.employee else None
+
+    @property
+    def total(self) -> float:
+        if not hasattr(self, "_total"):
+            self._total = sum(item.total for item in self.order_items)
+        return self._total
     
     @property
-    def employee_name(self):
-        return self.employee.person.name
-    
-    @property
-    def total(self):
-        return sum([item.total for item in self.order_items])
-    
-    def add_item(self, item: OrderItem, repository: Optional['IOrderRepository']) -> None:
-        if self.order_status.status not in [OrderStatusEnum.ORDER_PENDING.status]:
-            raise BadRequestException(message='Pedido não está pendente. Não é possível adicionar itens.')
-        
+    def is_paid(self) -> bool:
+        # TODO: Implementar lógica para verificar se o pagamento foi realizado
+        return True
+
+    def _validate_status(self, valid_statuses: List[OrderStatusEnum], action: str) -> None:
+        if self.order_status.status not in [status.status for status in valid_statuses]:
+            raise BadRequestException(f"O pedido não está em um estado válido para {action}.")
+
+    def _validate_new_status(self, new_status: OrderStatus, expected_status: OrderStatusEnum) -> None:
+        if new_status.status != expected_status.status:
+            raise BadRequestException(f"O status {new_status.status} é inválido. Esperado: {expected_status.status}.")
+
+    def add_item(self, item: OrderItem) -> None:
+        self._validate_status([OrderStatusEnum.ORDER_PENDING], "adicionar itens")
         self.order_items.append(item)
-        
-        if repository:
-            repository.update(self)
 
-    def remove_item(self, item: OrderItem, repository: Optional['IOrderRepository']) -> None:
-        if self.order_status.status not in [OrderStatusEnum.ORDER_PENDING.status]:
-            raise BadRequestException(message='Pedido não está pendente. Não é possível adicionar itens.')
-
+    def remove_item(self, item: OrderItem) -> None:
+        self._validate_status([OrderStatusEnum.ORDER_PENDING], "remover itens")
         self.order_items.remove(item)
 
-        if repository:
-            repository.update(self)
+    def place_order(self, new_status: OrderStatus) -> None:
+        self._validate_status([OrderStatusEnum.ORDER_PENDING], "realizar o pedido")
+        self._validate_new_status(new_status, OrderStatusEnum.ORDER_PLACED)
+        self.order_status = new_status
 
-    def place_order(self, order_status_repository: IOrderStatusRepository, repository: Optional['IOrderRepository']) -> None:
-        if self.order_status.status not in [OrderStatusEnum.ORDER_PENDING.status]:
-            raise BadRequestException(message='Pedido não está pendente. Não é possível realizar o pedido.')
-        
-        order_status = order_status_repository.get_by_status(OrderStatusEnum.ORDER_PLACED.status)
-        self.order_status = order_status
-        
-        if repository:
-            repository.update(self)
+    def cancel_order(self, new_status: OrderStatus) -> None:
+        self._validate_status([OrderStatusEnum.ORDER_PENDING, OrderStatusEnum.ORDER_PLACED], "cancelar o pedido")
+        self._validate_new_status(new_status, OrderStatusEnum.ORDER_CANCELLED)
+        self.order_status = new_status
 
-    def cancel_order(self, order_status_repository: IOrderStatusRepository, repository: Optional['IOrderRepository']) -> None:
-        # TODO: adicionar validação com status do pagamento. Caso o pagamento já tenha sido realizado, não é possível cancelar o pedido.
-        if self.order_status.status not in [OrderStatusEnum.ORDER_PENDING.status, OrderStatusEnum.ORDER_PLACED.status]:
-            raise BadRequestException(message='Pedido não está pendente ou realizado. Não é possível cancelar o pedido.')
+    def prepare_order(self, employee: Employee, new_status: OrderStatus) -> None:
+        self._validate_status([OrderStatusEnum.ORDER_PLACED], "preparar o pedido")
+        self._validate_new_status(new_status, OrderStatusEnum.ORDER_PREPARING)
         
-        order_status = order_status_repository.get_by_status(OrderStatusEnum.ORDER_CANCELLED.status)
-        self.order_status = order_status
+        if not self.is_paid:
+            raise BadRequestException("O pedido ainda não foi pago. Não é possível preparar o pedido.")
         
-        if repository:
-            repository.update(self)
-    
-    def prepare_order(self, order_status_repository: IOrderStatusRepository, repository: Optional['IOrderRepository']) -> None:
-        # TODO: adicionar validação com status do pagamento. Se não houver pagamento, não é possível preparar o pedido.
-        if self.order_status.status not in [OrderStatusEnum.ORDER_PLACED.status]:
-            raise BadRequestException(message='Pedido não está realizado. Não é possível preparar o pedido.')
-        
-        order_status = order_status_repository.get_by_status(OrderStatusEnum.ORDER_PREPARING.status)
-        self.order_status = order_status
-        
-        if repository:
-            repository.update(self)
-    
-    def ready_order(self, order_status_repository: IOrderStatusRepository, repository: Optional['IOrderRepository']) -> None:
-        if self.order_status.status not in [OrderStatusEnum.ORDER_PREPARING.status]:
-            raise BadRequestException(message='Pedido não está sendo preparado. Não é possível finalizar o pedido.')
-        
-        order_status = order_status_repository.get_by_status(OrderStatusEnum.ORDER_READY.status)
-        self.order_status = order_status
-        
-        if repository:
-            repository.update(self)
+        if not employee:
+            raise BadRequestException("É necessário um funcionário para preparar o pedido.")
 
-    def complete_order(self, order_status_repository: IOrderStatusRepository, repository: Optional['IOrderRepository']) -> None:
-        if self.order_status.status not in [OrderStatusEnum.ORDER_READY.status]:
-            raise BadRequestException(message='Pedido não está pronto. Não é possível finalizar o pedido.')
-        
-        order_status = order_status_repository.get_by_status(OrderStatusEnum.ORDER_COMPLETED.status)
-        self.order_status = order_status
-        
-        if repository:
-            repository.update(self)
+        self.id_employee = employee.id
+        self.employee = employee
+        self.order_status = new_status
+
+    def mark_as_ready(self, new_status: OrderStatus) -> None:
+        self._validate_status([OrderStatusEnum.ORDER_PREPARING], "finalizar o preparo do pedido")
+        self._validate_new_status(new_status, OrderStatusEnum.ORDER_READY)
+        self.order_status = new_status
+
+    def complete_order(self, new_status: OrderStatus) -> None:
+        self._validate_status([OrderStatusEnum.ORDER_READY], "completar o pedido")
+        self._validate_new_status(new_status, OrderStatusEnum.ORDER_COMPLETED)
+        self.order_status = new_status
+
+    def next_step(self, new_status: Optional[OrderStatus] = None, employee: Optional[Employee] = None) -> None:
+        current_status = OrderStatusEnum(self.order_status.status)
+
+        if current_status not in STATUS_TRANSITIONS:
+            raise BadRequestException(f"O estado atual {current_status.status} não permite transições.")
+
+        expected_next_status = STATUS_TRANSITIONS[current_status]
+
+        if expected_next_status == OrderStatusEnum.ORDER_PREPARING:
+            if not self.is_paid:
+                raise BadRequestException("O pedido ainda não foi pago. Não é possível preparar o pedido.")
+            
+            if not employee:
+                raise BadRequestException("É necessário um funcionário para preparar o pedido.")
+
+            self.id_employee = employee.id
+            self.employee = employee
+
+        if new_status:
+            self._validate_new_status(new_status, expected_next_status)
+            self.order_status = new_status
+        else:
+            self.order_status.status = expected_next_status.status
 
     __all__ = ['Order']
