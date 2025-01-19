a139deb5dc4d6710cce1988c7f9cee28dd78f095
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-01-19 14:13:33 -0300

    chore: populate user table

---
+++ migrations/versions/97h5618569bf_populate_user_table.py
@@ -0,0 +1,57 @@
+"""populate user table
+
+Revision ID: 97h5618569bf
+Revises: 97g5618569bf
+Create Date: 2025-01-19 13:31:12.951580
+
+"""
+
+import os
+import bcrypt
+from typing import Sequence, Union
+from alembic import op
+from sqlalchemy.sql import table, column, select
+from sqlalchemy import String, Integer
+from src.core.domain.entities.user import User
+from src.core.domain.dtos.user.user_dto import UserDTO
+
+# Revisão e informações básicas da migração
+revision = '97h5618569bf'
+down_revision: Union[str, None] = '97g5618569bf'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+users_table = table(
+    'users',
+    column('id', Integer),
+    column('name', String),
+    column('password_hash', String)
+)
+
+
+users = [
+    {
+        'name': 'joao.silva', 
+        'password_hash': bcrypt.hashpw('joao.silva'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    },
+    {
+        'name': 'carla.pereira',
+        'password_hash': bcrypt.hashpw('carla.pereira'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    },
+    {
+        'name': 'paulo.santos',
+        'password_hash': bcrypt.hashpw('paulo.santos'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    }
+]
+    
+
+def upgrade():
+    if os.getenv("ENVIRONMENT") == "testing":
+        return
+    
+    op.bulk_insert(users_table, users)
+
+def downgrade():
+    op.execute(
+        "DELETE FROM users WHERE name IN ('joao.silva', 'carla.pereira', 'paulo.santos')"
+    )
