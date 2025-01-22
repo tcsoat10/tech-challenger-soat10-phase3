79e65eea9ed57168827d05087bb48f34e1886b8e
Carlos Roberto <carlosjr.if@gmail.com>
2025-01-22 14:59:35 -0300

    chore(code-removal): remove unused routes payment

--- src/adapters/driver/api/v1/routes/payment_routes.py
+++ src/adapters/driver/api/v1/routes/payment_routes.py
@@ -13,10 +13,8 @@ from src.adapters.driven.repositories.payment_method_repository import PaymentMe
 from src.core.ports.payment_status.i_payment_status_repository import IPaymentStatusRepository
 from src.adapters.driven.repositories.payment_status_repository import PaymentStatusRepository
 from src.application.services.payment_service import PaymentService
-from src.core.domain.dtos.payment.payment_dto import PaymentDTO
 from src.core.auth.dependencies import get_current_user
 from src.constants.permissions import PaymentPermissions
-from src.core.domain.dtos.payment.update_payment_dto import UpdatePaymentDTO
 from src.core.ports.payment.i_payment_gateway import IPaymentGateway
 from src.adapters.driven.payment_providers.mercado_pago_gateway import MercadoPagoGateway
 from src.adapters.driven.repositories.order_repository import OrderRepository
@@ -43,90 +41,6 @@ def _get_payment_service(db_session: Session = Depends(get_db)) -> IPaymentServi
         order_repository,
         order_status_repository
     )
-
-
-@router.get(
-    '/payments/{payment_id}/id',
-    response_model=PaymentDTO,
-    status_code=status.HTTP_200_OK,
-    dependencies=[Security(get_current_user, scopes=[PaymentPermissions.CAN_VIEW_PAYMENTS])]
-)
-def get_payment_by_id(
-    payment_id: int,
-    service: IPaymentService = Depends(_get_payment_service),
-    user: dict = Security(get_current_user)
-):
-    return service.get_payment_by_id(payment_id)
-
-
-@router.get(
-    '/payments/{method_id}/method_id',
-    response_model=List[PaymentDTO],
-    status_code=status.HTTP_200_OK,
-    dependencies=[Security(get_current_user, scopes=[PaymentPermissions.CAN_VIEW_PAYMENTS])]
-)
-def get_payments_by_method_id(
-    method_id: int,
-    service: IPaymentService = Depends(_get_payment_service),
-    user: dict = Security(get_current_user)
-):
-    return service.get_payments_by_method_id(method_id)
-
-
-@router.get(
-    '/payments/{status_id}/status_id',
-    response_model=List[PaymentDTO],
-    status_code=status.HTTP_200_OK,
-    dependencies=[Security(get_current_user, scopes=[PaymentPermissions.CAN_VIEW_PAYMENTS])]
-)
-def get_payments_by_status_id(
-    status_id: int,
-    service: IPaymentService = Depends(_get_payment_service),
-    user: dict = Security(get_current_user)
-):
-    return service.get_payments_by_status_id(status_id)
-
-
-@router.get(
-    '/payments',
-    response_model=List[PaymentDTO],
-    status_code=status.HTTP_200_OK,
-    dependencies=[Security(get_current_user, scopes=[PaymentPermissions.CAN_VIEW_PAYMENTS])]
-)
-def get_all_payments(
-    include_deleted: bool = False,
-    service: IPaymentService = Depends(_get_payment_service),
-    user: dict = Security(get_current_user)
-):
-    return service.get_all_payments(include_deleted=include_deleted)
-
-
-@router.put(
-    '/payments/{payment_id}',
-    response_model=PaymentDTO,
-    status_code=status.HTTP_200_OK,
-    dependencies=[Security(get_current_user, scopes=[PaymentPermissions.CAN_UPDATE_PAYMENT])]
-)
-def update_payment(
-    payment_id: int,
-    dto: UpdatePaymentDTO,
-    service: IPaymentService = Depends(_get_payment_service),
-    user: dict = Security(get_current_user)
-):
-    return service.update_payment(payment_id, dto)
-
-
-@router.delete(
-    '/payments/{payment_id}',
-    status_code=status.HTTP_204_NO_CONTENT,
-    dependencies=[Security(get_current_user, scopes=[PaymentPermissions.CAN_DELETE_PAYMENT])]
-)
-def delete_payment(
-    payment_id: int,
-    service: IPaymentService = Depends(_get_payment_service),
-    user: dict = Security(get_current_user)
-):
-    return service.delete_payment(payment_id)
 
 # Criar pagamento para o pedido
 @router.post(
