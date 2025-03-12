92887742f96977fef71eacc00b73779a4c6b4e80
Carlos Roberto <carlosjr.if@gmail.com>
2025-03-12 12:32:23 -0300

    refactor(permission-controller): inject gateways instead of constructing via db connection

--- src/adapters/driver/api/v1/controllers/permission_controller.py
+++ src/adapters/driver/api/v1/controllers/permission_controller.py
@@ -1,8 +1,6 @@
-from sqlalchemy.orm import Session
 from typing import Optional, List
 
 from src.core.ports.permission.i_permission_repository import IPermissionRepository
-from src.adapters.driven.repositories.permission_repository import PermissionRepository
 from src.core.domain.dtos.permission.create_permission_dto import CreatePermissionDTO
 from src.core.domain.dtos.permission.permission_dto import PermissionDTO
 from src.application.usecases.permission_usecase.create_permission_usecase import CreatePermissionUsecase
@@ -15,8 +13,9 @@ from src.application.usecases.permission_usecase.delete_permission_usecase impor
 
 
 class PermissionController:
-    def __init__(self, db_connection: Session):
-        self.permission_gateway: IPermissionRepository = PermissionRepository(db_connection)
+
+    def __init__(self, permission_gateway: IPermissionRepository):
+        self.permission_gateway: IPermissionRepository = permission_gateway
     
     def create_permission(self, dto: CreatePermissionDTO) -> PermissionDTO:
         create_permission_usecase = CreatePermissionUsecase.build(self.permission_gateway)
