1cba755127ac52b07327bf965aabb0524f71512b
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-03-16 00:18:37 -0300

    refactor: inject product controller on product routes

--- src/adapters/driver/api/v1/routes/product_routes.py
+++ src/adapters/driver/api/v1/routes/product_routes.py
@@ -1,25 +1,17 @@
 from typing import Optional
 from fastapi import APIRouter, Depends, Query, Security, status
-from sqlalchemy.orm import Session
+from dependency_injector.wiring import inject, Provide
 
-from src.adapters.driven.repositories.category_repository import CategoryRepository
-from src.adapters.driven.repositories.product_repository import ProductRepository
-from src.core.ports.category.i_category_repository import ICategoryRepository
-from src.core.ports.product.i_product_repository import IProductRepository
 from src.adapters.driver.api.v1.controllers.product_controller import ProductController
-from config.database import get_db
 from src.constants.permissions import ProductPermissions
 from src.core.auth.dependencies import get_current_user
 from src.core.domain.dtos.product.update_product_dto import UpdateProductDTO
 from src.core.domain.dtos.product.create_product_dto import CreateProductDTO
 from src.core.domain.dtos.product.product_dto import ProductDTO
+from src.core.containers import Container
+
 
 router = APIRouter()
-
-def _get_product_controller(db_session: Session = Depends(get_db)):
-    product_gateway: IProductRepository = ProductRepository(db_session)
-    category_gateway: ICategoryRepository = CategoryRepository(db_session)
-    return ProductController(product_gateway, category_gateway)
 
 
 @router.post(
@@ -28,9 +20,10 @@ def _get_product_controller(db_session: Session = Depends(get_db)):
     status_code=status.HTTP_201_CREATED,
     dependencies=[Security(get_current_user, scopes=[ProductPermissions.CAN_CREATE_PRODUCT])]
 )
+@inject
 def create_product(
     dto: CreateProductDTO,
-    controller: ProductController = Depends(_get_product_controller),
+    controller: ProductController = Depends(Provide[Container.product_controller]),
     user=Depends(get_current_user)
 ):
     return controller.create_product(dto)
@@ -40,9 +33,10 @@ def create_product(
     response_model=ProductDTO, status_code=status.HTTP_200_OK,
     dependencies=[Security(get_current_user, scopes=[ProductPermissions.CAN_VIEW_PRODUCTS])]
 )
+@inject
 def get_product_by_name(
     product_name: str,
-    controller: ProductController = Depends(_get_product_controller),
+    controller: ProductController = Depends(Provide[Container.product_controller]),
     user=Depends(get_current_user)
 ):
     return controller.get_product_by_name(name=product_name)
@@ -53,9 +47,10 @@ def get_product_by_name(
     status_code=status.HTTP_200_OK,
     dependencies=[Security(get_current_user, scopes=[ProductPermissions.CAN_VIEW_PRODUCTS])]
 )
+@inject
 def get_product_by_id(
     product_id: int,
-    controller: ProductController = Depends(_get_product_controller),
+    controller: ProductController = Depends(Provide[Container.product_controller]),
     user=Depends(get_current_user)
 ):
     return controller.get_product_by_id(product_id=product_id)
@@ -66,10 +61,11 @@ def get_product_by_id(
     status_code=status.HTTP_200_OK,
     dependencies=[Security(get_current_user, scopes=[ProductPermissions.CAN_VIEW_PRODUCTS])]
 )
+@inject
 def get_all_products(
     include_deleted: Optional[bool] = Query(False),
     categories: Optional[list[str]] = Query(None),
-    controller: ProductController = Depends(_get_product_controller),
+    controller: ProductController = Depends(Provide[Container.product_controller]),
     user=Depends(get_current_user)
 ):
     return controller.get_all_products(categories=categories, include_deleted=include_deleted)
@@ -80,10 +76,11 @@ def get_all_products(
     status_code=status.HTTP_200_OK,
     dependencies=[Security(get_current_user, scopes=[ProductPermissions.CAN_UPDATE_PRODUCT])]
 )
+@inject
 def update_product(
     product_id: int,
     dto: UpdateProductDTO,
-    controller: ProductController = Depends(_get_product_controller),
+    controller: ProductController = Depends(Provide[Container.product_controller]),
     user=Depends(get_current_user)
 ):
     return controller.update_product(product_id, dto)
@@ -93,5 +90,6 @@ def update_product(
     status_code=status.HTTP_204_NO_CONTENT,
     dependencies=[Security(get_current_user, scopes=[ProductPermissions.CAN_DELETE_PRODUCT])]
 )
-def delete_product(product_id: int, controller: ProductController = Depends(_get_product_controller)):
+@inject
+def delete_product(product_id: int, controller: ProductController = Depends(Provide[Container.product_controller])):
     controller.delete_product(product_id)
--- src/core/containers.py
+++ src/core/containers.py
@@ -2,12 +2,17 @@ from dependency_injector import containers, providers
 from sqlalchemy.orm import Session
 from src.adapters.driver.api.v1.controllers.category_controller import CategoryController
 from src.adapters.driven.repositories.category_repository import CategoryRepository
+from src.adapters.driver.api.v1.controllers.product_controller import ProductController
+from src.adapters.driven.repositories.product_repository import ProductRepository
+
 
 class Container(containers.DeclarativeContainer):
 
     wiring_config = containers.WiringConfiguration(modules=[
         "src.adapters.driver.api.v1.controllers.category_controller",
         "src.adapters.driver.api.v1.routes.category_routes",
+        "src.adapters.driver.api.v1.controllers.product_controller",
+        "src.adapters.driver.api.v1.routes.product_routes",
     ])
 
     db_session = providers.Dependency(instance_of=Session)
@@ -20,4 +25,9 @@ class Container(containers.DeclarativeContainer):
     category_controller = providers.Factory(
         CategoryController,
         category_gateway=category_gateway
+    )
+
+    product_gateway = providers.Factory(ProductRepository, db_session=db_session)
+    product_controller = providers.Factory(
+        ProductController, product_gateway=product_gateway, category_gateway=category_gateway
     )
