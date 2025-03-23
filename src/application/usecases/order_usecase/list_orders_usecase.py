cce169b32179fad4f3f58502df2defa7fdcd1814
Carlos Roberto <carlosjr.if@gmail.com>
2025-03-23 19:58:14 -0300

    refactor: filter order with default status order_paid > order_preparing > order_ready

--- src/application/usecases/order_usecase/list_orders_usecase.py
+++ src/application/usecases/order_usecase/list_orders_usecase.py
@@ -2,6 +2,7 @@
 from typing import List
 from src.core.domain.entities.order import Order
 from src.core.ports.order.i_order_repository import IOrderRepository
+from src.constants.order_status import OrderStatusEnum
 
 
 class ListOrdersUseCase:
@@ -15,5 +16,13 @@ class ListOrdersUseCase:
         
     def execute(self, current_user: dict, status: List[str] = None) -> List[Order]:
         customer_id = int(current_user['person']['id']) if current_user['profile']['name'] == 'customer' else None
+        
+        if status is None:
+            status = [
+                OrderStatusEnum.ORDER_PAID.status, # Order paid by the customer
+                OrderStatusEnum.ORDER_PREPARING.status, # Order being prepared by the staff
+                OrderStatusEnum.ORDER_READY.status # Order ready for pickup
+            ]
+        
         orders = self.order_gateway.get_all(status=status, customer_id=customer_id)
         return orders
